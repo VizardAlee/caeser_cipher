4b1c20910eeb2d52059c6fb7d00ae26150f9df0b
Aliyu <biggy0023@gmail.com>
2022-09-10 22:26:32 +0100

    Finally went back once is understood the ruby concept more. This is much better

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # caeser_cipher
+
+
+This is a game which requries taking strings and encrypting them
+
+In the encryption process, you can use a "key" which is usually a number
+This key represents the number of shifts in the alphabet required for the encryption
+Then the word or phrase which needs encrypting
+The end result will be the encrypted code
+Conversely, retrieving the information encrypted, the retriever will need the key to decrypt the information
--- cipher.rb
+++ cipher.rb
@@ -1,27 +1,49 @@
-def caesar_cipher(string, shifts)
-  characters = ("a".."z").to_a #creates an array of strings from a-z
-  input = string.split("") #splits the string input letters within an array
-  encrypted = "" #empty string for the encryption
-  input.each do |char| #for each character
-    i = characters.index(char) #assign i as the index position (from characters) found in the input
-    shifted = (i + shifts) % characters.length #add it to the number of shifts given and ensure it's within the characters length
-    encrypted << characters[shifted] #append the shifted to the encrypted string variable
-  end
-  encrypted #return the ciphered text
+# frozen_string_literal: false
+
+# In the encryption process, you can use a "key" which is usually a number
+# This key represents the number of shifts in the alphabet required for the encryption
+# Then the word or phrase which needs encrypting
+# The end result will be the encrypted code
+# Conversely, retrieving the information encrypted, the retriever will need the key to decrypt the information
+
+# method to convert string to number representation
+def string_to_number(str)
+  splitting = str.split('')
+  numberfy = splitting.map { |value| value.ord }
+  numberfy.join(' ')
 end
 
-p caesar_cipher("aliyu", 3)
-
-def decrypt(encryption, shifts)
-  characters = ("a".."z").to_a
-  input = encryption.split('')
-  decryption = ""
-  input.each do |char|
-    i = characters.index(char)
-    shifted_back = (i - shifts) % characters.length #all we had to do was change the plus to minus
-    decryption << characters[shifted_back]
-  end
-  decryption
+# method to convert number to new encrypted string
+def number_to_string(number)
+  to_word = number.map { |value| value.chr }
+  to_word.join
 end
 
-p decrypt("dolbx", 3)
+# method that sums the key value with the number
+def keyington(string, key)
+  arrayfy = string_to_number(string).split
+  intergerize = arrayfy.map { |value| value.to_i + key }
+end
+
+# method that brings everything together for full encryption
+def caesar_cipher(value, key)
+  number = keyington(value, key)
+  number_to_string(number)
+end
+
+puts caesar_cipher('Bismillah! My name is Aliyu', 5) # if there's 'z' alphabet in the code, don't exceed key = 5
+
+# simple method to convert evncryption to number
+def encryption_to_number(value)
+  value.map { |code| code.ord }
+end
+
+# full decryption method
+def decrypt_cipher(value, key)
+  arrayfy = value.split('')
+  code = encryption_to_number(arrayfy)
+  decrypt = code.map { |value| value - key}
+  number_to_string(decrypt)
+end
+
+puts decrypt_cipher('Gnxrnqqfm&%R~%sfrj%nx%Fqn~z', 5) # it worked!
